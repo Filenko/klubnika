85a7417e62366c2934dd5c48c5775defa9d75ba2
Filenko Timofey <timofey.filenko@yandex.ru>
2022-07-02 22:47:35 +0300

    db added

--- main.py
+++ main.py
@@ -1,47 +1,35 @@
 import telebot
 from flask import Flask , render_template, jsonify, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import os
 
 bot = telebot.TeleBot("5208570240:AAGQmP5plGxVk8h8MSsRVy_d8gKhiRlaUXc")
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://esftupvernqgde:d4580dafa8a9d85ff2fa1627d3a91c2da447f4945c84a3f48bf291a7856a2b36@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d934qinsg9rah8'
-db = SQLAlchemy(app)
 
-class User(db.Model):
-
-    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
-    pull_up = db.Column(db.Integer)
-    press = db.Column(db.Integer)
-
-
-    def __init__(self, user_idd, press, pull_up, name):
-        self.user_id = user_idd
-        self.press = press
-        self.pull_up = pull_up
-
+DB_URI = 'postgres://sxszeitvskatkt:252fcca6ea55cf02d40f2f9b8bbbe10d534d55b546b37764c37ad178a66ccfca@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d9pu6bj42i81g3'
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 
-    usr = User.query.filter_by(user_id = message.from_user.id).first()
-    tg_usr = message.from_user
+    user_id = message.from_user.id
+    username = tg_usr.username if tg_usr.username != None else '-' 
+    bot.reply_to(message, f"Hello, {username}!")
 
-    if usr is None:
-        first = tg_usr.first_name if tg_usr.first_name != None else '-' 
-        user_id = message.from_user.id
-        new_data = User(user_id, 0, 0)
-        db.session.add(new_data)
-        bot.send_message(message.chat.id, f'Привет! Вы добавлены в базу, {first}!')
-    db.session.commit()
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
+
+    if not result:
+        db_object.execute("INSERT INTO users(user_id, username, pull_up, press) VALUES (%s, %s, %s, %s)", (user_id, username, 0, 0))
+        db_connection.commit()
+
+    update_messages_count(user_id)
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
 @app.route("/")
 def home():
