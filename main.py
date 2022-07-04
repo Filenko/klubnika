9a66890cb7594b2815f1ea0905bf3478f176b659
Filenko Timofey <timofey.filenko@yandex.ru>
2022-07-05 00:07:43 +0300

    sort pull_ups

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ def get_stats(message):
         result = result[::-1]
         reply_message = "Top:\n"
         for i, item in enumerate(result):
-            reply_message += f"{i + 1}. {item[1].strip()} has {item[2]} анжуманя and {item[3]} прес\n"
+            flag = ''
+            if item[2] >= 100 and item[3] >= 100:
+                flag = U+2705
+            reply_message += f"{flag}{i + 1}. {item[1].strip()} has {item[2]} анжуманя and {item[3]} прес\n"
         bot.reply_to(message, reply_message)
 
 
